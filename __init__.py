637da83af56818acf1e1c049fa0ff1ad310d7951
jai-git4208 <jaiminpansal@gmail.com>
2025-10-27 16:20:12 +0530

    refractor

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .core import encrypt, decrypt, FuckYouCrypto
-from .encoder import FuckYouEncoder
-from .noise import add_noise, remove_noise
-
-__version__ = '1.0.0'
-__all__ = ['encrypt', 'decrypt', 'FuckYouCrypto', 'FuckYouEncoder', 'add_noise', 'remove_noise']
